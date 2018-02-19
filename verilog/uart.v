5e9e4043415770975e287604b0b491e9b330c11d
Alex McLain <alex@alexmclain.com>
2018-02-18 20:21:20 -0800

    Implement uart_tx.

--- verilog/uart.v
+++ verilog/uart.v
@@ -2,6 +2,9 @@ module Uart (
   input reset_i,
   input clock_i,
   input write_i,
+  input two_stop_bits_i,
+  input parity_bit_i,
+  input parity_even_i,
   input [7:0] data_i,
   output busy_o,
   output serial_o
@@ -13,8 +16,11 @@ UartTx uart_tx
 (
   .reset_i(reset_i),
   .clock_i(clock_i),
-  .clock_divider_i(clock_divider_i),
   .write_i(write_i),
+  .two_stop_bits_i(two_stop_bits_i),
+  .parity_bit_i(parity_bit_i),
+  .parity_even_i(parity_even_i),
+  .clock_divider_i(clock_divider_i),
   .data_i(data_i),
   .busy_o(busy_o),
   .serial_o(serial_o)
--- verilog/uart_tx.v
+++ verilog/uart_tx.v
@@ -4,8 +4,11 @@ module UartTx #(
 (
   input reset_i,
   input clock_i,
-  input [CLOCK_DIVIDER_WIDTH - 1:0] clock_divider_i,
   input write_i,
+  input two_stop_bits_i,
+  input parity_bit_i,
+  input parity_even_i,
+  input [CLOCK_DIVIDER_WIDTH - 1:0] clock_divider_i,
   input [7:0] data_i,
   output reg serial_o = 1'b1,
   output busy_o
@@ -13,27 +16,53 @@ module UartTx #(
 
 localparam STATE_POST_RESET = 0;
 localparam STATE_IDLE = 1;
-localparam STATE_SEND_START_BIT = 2;
-localparam STATE_SEND_DATA_BIT = 3;
-localparam STATE_SEND_STOP_BIT = 4;
+localparam STATE_SEND_PACKET = 2;
 
-reg [2:0] state = STATE_POST_RESET;
+reg [1:0] state = STATE_POST_RESET;
 reg [CLOCK_DIVIDER_WIDTH - 1:0] bit_timer = 0;
-reg [3:0] select_data_bit = 0;
+reg [5:0] select_packet_bit = 0;
 reg [7:0] data = 8'h00;
+reg two_stop_bits = 1'b0;
+reg parity_bit = 1'b0;
+reg parity_even = 1'b0;
+reg write_has_triggered = 1'b0;
 
+wire [11:0] packet;
 wire [CLOCK_DIVIDER_WIDTH - 1:0] clock_divider;
+wire [3:0] total_bits_to_send = 4'd10 + two_stop_bits + parity_bit;
+wire even_parity_value =
+  1'h01 & (
+    packet[8] +
+    packet[7] +
+    packet[6] +
+    packet[5] +
+    packet[4] +
+    packet[3] +
+    packet[2] +
+    packet[1]
+  );
 
 assign busy_o = (state == STATE_IDLE && !reset_i) ? 1'b0 : 1'b1;
 assign clock_divider = (clock_divider_i > 0) ? clock_divider_i - 1'd1 : 1'd0;
 
+assign packet[0] = 1'b0; // Start bit
+assign packet[8:1] = data;
+assign packet[9] = parity_bit
+  ? (parity_even ? even_parity_value : ~even_parity_value) // Parity bit
+  : 1'b1; // Stop bit
+assign packet[11:10] = 2'b11; // Definitely stop bits
+
 always @ (posedge reset_i, posedge clock_i) begin
   if (reset_i) begin
     state <= STATE_POST_RESET;
-    bit_timer <= 0;
-    select_data_bit <= 0;
-    data <= 8'h00;
     serial_o <= 1'b1;
+    bit_timer <= 0;
+    select_packet_bit <= 0;
+    data <= 8'h00;
+    two_stop_bits <= 1'b0;
+    parity_bit <= 1'b0;
+    parity_even <= 1'b0;
+    write_has_triggered <= 1'b0;
   end
   else begin
     case (state)
@@ -44,10 +73,10 @@ always @ (posedge reset_i, posedge clock_i) begin
         if (bit_timer < clock_divider) begin
           bit_timer <= bit_timer + 1'd1;
         end
-        // Reusing `select_data_bit` as an overall bit counter for the packet.
-        else if (select_data_bit <= 10) begin
+        // Reusing `select_packet_bit` as an overall bit counter for the packet.
+        else if (select_packet_bit < 12) begin
           bit_timer <= 0;
-          select_data_bit <= select_data_bit + 1'd1;
+          select_packet_bit <= select_packet_bit + 1'd1;
         end
         else begin
           state <= STATE_IDLE;
@@ -55,58 +84,39 @@ always @ (posedge reset_i, posedge clock_i) begin
 
       STATE_IDLE:
         begin
-          bit_timer <= 0;
-          select_data_bit <= 0;
           serial_o <= 1'b1;
+          bit_timer <= 0;
+          select_packet_bit <= 0;
 
-          if (write_i) begin
+          if (!write_i)
+            write_has_triggered <= 1'b0;
+
+          if (write_i && !write_has_triggered) begin
             data <= data_i;
-            state <= STATE_SEND_START_BIT;
+            two_stop_bits <= two_stop_bits_i;
+            parity_bit <= parity_bit_i;
+            parity_even <= parity_even_i;
+            write_has_triggered <= 1'b1;
+
+            state <= STATE_SEND_PACKET;
           end
         end
 
-      STATE_SEND_START_BIT:
-        begin
-          serial_o <= 1'b0;
+      STATE_SEND_PACKET:
+        if (select_packet_bit < total_bits_to_send) begin
+          serial_o <= packet[select_packet_bit];
 
           if (bit_timer < clock_divider) begin
             bit_timer <= bit_timer + 1'd1;
           end
           else begin
             bit_timer <= 0;
-            state <= STATE_SEND_DATA_BIT;
-          end
-        end
-
-      STATE_SEND_DATA_BIT:
-        if (select_data_bit <= 4'd7) begin
-          serial_o <= data[select_data_bit];
-
-          if (bit_timer < clock_divider) begin
-            bit_timer <= bit_timer + 1'd1;
-          end
-          else begin
-            bit_timer <= 0;
-            select_data_bit <= select_data_bit + 1'd1;
+            select_packet_bit <= select_packet_bit + 1'd1;
           end
         end
         else begin
-          serial_o <= 1'b1;
           bit_timer <= 0;
-          state <= STATE_SEND_STOP_BIT;
-        end
-
-      STATE_SEND_STOP_BIT:
-        begin
-          serial_o <= 1'b1;
-
-          if (bit_timer < clock_divider) begin
-            bit_timer <= bit_timer + 1'd1;
-          end
-          else begin
-            bit_timer <= 0;
-            state <= STATE_IDLE;
-          end
+          state <= STATE_IDLE;
         end
 
       default:
